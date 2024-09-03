fc76a86f1a2cac210b3700cc9f0e156a31461130
Marto <martinemarzorati@gmail.com>
2024-09-03 12:32:58 +0200

    refactor main

--- main.py
+++ main.py
@@ -1,46 +1,32 @@
+from typing import Counter
+
+
+url = "books/frankenstein.txt"
+
+
 def main():
-    with open("books/frankenstein.txt") as f:
+    with open(url) as f:
         text = f.read()
-        #print (content)
+        #print (text)
+        count_words(text)
+        count_charac(text)
+        report(count_charac)
         return text
     
-book = main()
+def count_words(text):
+    words = text.split()
+    word_count = len(words)
+    return word_count
+    
+def count_charac (text):
+    lowered_string = text.lower()
+    character_counter = Counter(lowered_string)
+    list_of_charact = [{count,char}for char, count in character_counter.items()]
+    #print (list_of_charact)
+    return list_of_charact
 
+def report (list):
+    order_list = list
+    print(order_list)
 
-    
-def count_words(book):
-    counter = 0
-    words_count = book.split()
-    for words in words_count:
-        counter += 1
-    
-    #print (counter)
-    
-def charac_count(book):
-    charac_lowered = book.lower()
-    characters = list(charac_lowered)
-    unique_characters = set(characters)
-    #print (unique_characters)
-    charact_list = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","y","z"]
-    charac_counter = {}
-    #print(characters)
-    
-    for charac in characters:
-        counter = 0
-        if charac in charact_list:
-            counter += 1
-            charac_counter[charac] += counter
-    print(charac_counter)
-    
-            
-            
-        
-    
-charac_count(book)
-    
-    
-count_words(book)
-    
-
-    
-
+main() 
